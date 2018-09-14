d562ff98ebf7c161c3212342fab5df5a5d928d48
danielflopez <daniel.f.lopez.j@gmail.com>
2018-09-14 19:15:54 +0000

    cambios para el servidor

--- blockchain_api_python/blockchain_api_python/settings.py
+++ blockchain_api_python/blockchain_api_python/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4!kftrzu6+i!2ba1%9ly27l4b@!9tk0_a&5j$i6u9z)-1(qwa%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['142.93.113.157']
 
 
 # Application definition
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
